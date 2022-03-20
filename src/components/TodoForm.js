e6f89f8f78b6d4a51d5b9aec3e316f5800747817
rumikoacopa <rumikorios@gmail.com>
2022-03-20 00:03:23 -0400

    try to stop page from refreshing, add handleSubmit e.preventDefault, added onSubmit to form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,8 +3,17 @@ import React, { useState } from "react";
 function TodoForm() {
   const [input, setInput] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+  };
+
   return (
-    <form className='todo-form'>
+    <form className='todo-form' onSubmit={handleSubmit}>
       <input
         type='text'
         placeholder='Add a todo'
