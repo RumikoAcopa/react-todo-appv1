6b01f35df11fb8ead3d11990d61c8c1a9b2a34dd
rumikoacopa <rumikorios@gmail.com>
2022-03-20 00:16:18 -0400

    using empty string in setInput for now to allow us to type in the input and not get props error

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,15 +3,22 @@ import React, { useState } from "react";
 function TodoForm() {
   const [input, setInput] = useState("");
 
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    props.onSubmit({
-      id: Math.floor(Math.random() * 10000),
-      text: input
-    });
-  };
+    // props.onSubmit({
+    //   id: Math.floor(Math.random() * 10000),
+    //   text: input
+    // });
 
+    setInput('');
+  };
+  //using props because we want to pass
+  //generates random number in a 10k range, chances of getting the same id will be less.
   return (
     <form className='todo-form' onSubmit={handleSubmit}>
       <input
@@ -20,6 +27,7 @@ function TodoForm() {
         value={input}
         name='text'
         className='todo-input'
+        onChange={handleChange}
       />
       <button className='todo-button'>Add todo</button>
     </form>
